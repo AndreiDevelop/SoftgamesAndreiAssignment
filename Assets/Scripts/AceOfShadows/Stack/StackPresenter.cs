8dcbd8c7f3a66b7c4bed7f28d47305ab1067a8e9
Andrii Maksymov <making714@gmail.com>
2025-05-16 19:39:20 +0300

    counters updated

---
+++ Assets/Scripts/AceOfShadows/Stack/StackPresenter.cs
@@ -0,0 +1,45 @@
+using SoftgamesAssignment.Card;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace SoftgamesAssignment
+{
+    public class StackPresenter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro _textCounter;
+        [SerializeField] private bool _inverseCounter;
+        
+        [Inject] private CardModel _cardModel;
+        
+        private int _cardsCount;
+        private int _currentShowCardCount;
+        
+        public void Start()
+        {
+            _cardsCount = _cardModel.Deck.Count;
+            _currentShowCardCount = 0;
+            UpdateCounter();
+            
+            _cardModel.OnGetTopCard
+                .Subscribe(cardOrder =>
+                {
+                    _currentShowCardCount++;
+                    UpdateCounter();
+                }).AddTo(this);
+        }
+
+        private void UpdateCounter()
+        {
+            if (_inverseCounter)
+            {
+                _textCounter.text = $"{_cardsCount - _currentShowCardCount} / {_cardsCount}";
+            }
+            else
+            {
+                _textCounter.text = $"{_currentShowCardCount} / {_cardsCount}";
+            }
+        }
+    }
+}
